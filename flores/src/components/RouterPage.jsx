26e7c273cad6265f3b2bacae697ec1e9c1d0591f
Matías <dmcabrera90@gmail.com>
2022-03-13 15:07:12 -0300

    main

--- flores/src/components/RouterPage.jsx
+++ flores/src/components/RouterPage.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Flores from './Flores'
 import About from './About'
-import '../assets/css/style.css';
 
 export default function RouterPage() {
-  return (
-      <Router>
-          <Routes>
-              <Route path='/' exact component={Flores} />
-              <Route path='/about' component={About} />
-          </Routes>
-      </Router>
-  )
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' exact element={<Flores />} />
+                <Route path='/about' exact element={<About />} />
+            </Routes>
+        </Router>
+    )
 }
